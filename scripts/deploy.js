949a2dbb51cf43d884992f2bc69882f25825d7ec
eduwr <eduwric@gmail.com>
2021-07-31 21:09:45 -0300

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+  const Sample = await ethers.getContractFactory("Sample");
+  const sampleContract = await Sample.deploy("My Sample Contract");
+
+  console.log("My Contract deployed to:", sampleContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ test/Sample.test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+
+describe("Sample", () => {
+  let sample;
+  const INITIAL_NAME = "My Sample Contract";
+
+  before(async () => {
+    const Sample = await ethers.getContractFactory("Sample");
+    sample = await Sample.deploy(INITIAL_NAME);
+
+    await sample.deployed();
+  });
+
+  it("should deploy the contract with initial name", async () => {
+    expect(await sample.getName()).to.equal(INITIAL_NAME);
+  });
+
+  it("should change the name when requested", async () => {
+    await sample.changeName("NEW NAME");
+
+    const name = await sample.getName();
+    expect(name).to.equal("NEW NAME");
+  });
+});
